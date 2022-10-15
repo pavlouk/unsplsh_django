0e6da7d5f76837c5ab6371fe39305dc262d2e8de
ploukareas <ploukareas@gmail.com>
2022-10-15 14:28:25 +0300

    init

---
+++ unsplsh/snippets/external.py
@@ -0,0 +1,41 @@
+from io import BytesIO
+
+import httpx
+from PIL import Image
+from unsplsh.settings import UNSPLASH_KEY, UNSPLASH_URL
+
+
+def unsplash_builder(search) -> str:
+    search_url = UNSPLASH_URL + f"?query={search.query}"
+    if search.orientation is not None:
+        search_url += f"&orientation={search.orientation.value}"
+    if search.color is not None:
+        search_url += f"&color={search.color.value}"
+    search_url += f"&client_id={UNSPLASH_KEY}"
+    return search_url
+
+
+def create_thumbnail(buffered_image: BytesIO):
+    image = Image.open(buffered_image)
+    image.thumbnail(
+        size=(200, image.height * 200 // image.width), resample=Image.Resampling.LANCZOS
+    )
+    buffered_output = BytesIO()
+    image.save(buffered_output, format=image.format)
+    thumbnail_string = buffered_output.getvalue()
+    return thumbnail_string
+
+
+async def fetch_image(search_url: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(search_url)
+        response.raise_for_status()
+        response_body = response.json()
+        search_results = response_body.get("results", {})
+        urls = search_results[0].get("urls", {})
+        raw_url = urls.get("small", "Error")
+        image_response = await client.get(raw_url)
+        response.raise_for_status()
+        image_buffer = BytesIO(image_response.content)
+        thumbnail_string = create_thumbnail(image_buffer)
+        return thumbnail_string
